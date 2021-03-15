a4ddf35928a8385682d72e8d002efd1532208fdf
Artur Kempny <artur.kempny@gmail.com>
2021-03-15 20:09:39 +0100

    create sidebar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import Mail from './Mail';
+import EmailList from './EmailList';
 import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Sidebar />
+      <Router>
+        <Header />
+        <div className="app__body">
+          <Sidebar />
+          <Switch>
+              <Route path="/mail">
+                <Mail />
+              </Route>
+              <Route path="/">
+                <EmailList />
+              </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
