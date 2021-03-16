45af041586d3db91e0a981d3d8d9ec20196ead2e
Artur Kempny <artur.kempny@gmail.com>
2021-03-16 09:22:38 +0100

    create email row

--- src/EmailList.js
+++ src/EmailList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Section from './Section';
+import EmailRow from './EmailRow';
 import { Checkbox, IconButton } from '@material-ui/core'
 import { ArrowDropDown, ChevronLeft, ChevronRight, Inbox, KeyboardHide, LocalOffer, MoreVert, People, Redo, Settings } from '@material-ui/icons'
 import './EmailList.css'
@@ -39,6 +40,14 @@ const EmailList = () => {
                 <Section Icon={Inbox} title="primary" color="red" selected />
                 <Section Icon={People} title="Social" color="#1A73E8" />
                 <Section Icon={LocalOffer} title="Promotions" color="green" />
+            </div>
+            <div className="emailList__list">
+                <EmailRow 
+                    title="Twitch"
+                    subject="Hey stremer!!"
+                    description="Test message"
+                    time="10pm"
+                />
             </div>
         </div>
     )
